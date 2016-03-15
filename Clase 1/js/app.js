f70e84a17fad7b641f2a5a5346c8a5adb3be7a21
EduardoSP <eduardo.saavedra@correounivalle.edu.co>
2016-03-15 11:35:31 -0500

    Agregando clase 1

---
+++ Clase 1/js/app.js
@@ -0,0 +1,46 @@
+/*
+(function(){
+	var app = angular.module('store',[]);
+
+	app.controller('StoreController', function(){
+		this.product = gem;
+	});
+
+	var gem ={
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: '...',
+		canPurchase: true
+
+	}
+
+})();
+*/
+(function(){
+	var app = angular.module('store',[]);
+
+	app.controller('StoreController', function(){
+		this.products = gems;
+	});
+
+	var gems =[
+	{
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: '...',
+		canPurchase: false,
+
+	},
+
+	{
+		name: 'Pentagonal Gem',
+		price: 5.95,
+		description: '...',
+		canPurchase: true,
+
+	}
+
+	];
+
+
+})();
